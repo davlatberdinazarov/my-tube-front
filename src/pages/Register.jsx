f65a114d9f03fb037e7bc34854bcee55acfbc435
davlatberdinazarov <aberdinazarov71@gmail.com>
2025-04-07 17:10:35 +0500

    added register axios

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,36 +3,62 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log(name, email, password);
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://my-shop-api-n5vp.onrender.com/api/auth/register",
+        {
+          name,
+          email,
+          password,
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h2 className="text-2xl font-bold mb-4">Register</h2>
       <form
+        onSubmit={handleRegister}
         className="bg-white p-6 rounded-lg shadow-md max-w-lg"
       >
         <input
           type="text"
           placeholder="Name"
           className="w-full p-2 border rounded mb-2"
+          onChange={(e) => setName(e.target.value)}
           required
         />
         <input
           type="email"
           placeholder="Email"
           className="w-full p-2 border rounded mb-2"
+          onChange={(e) => setEmail(e.target.value)}
           required
         />
         <input
           type="password"
           placeholder="Password"
           className="w-full p-2 border rounded mb-2"
+          onChange={(e) => setPassword(e.target.value)}
           required
         />
         <button
           type="submit"
-          className="w-full bg-blue-500 text-white p-2 rounded"
+          className="w-full bg-green-500 text-white p-2 rounded"
         >
-          Login
+          Register
         </button>
       </form>
       <p className="mt-2">
@@ -45,5 +71,4 @@ const Register = () => {
   );
 };
 
-
-export default Register
+export default Register;
---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+// BASE_URL https://my-shop-api-n5vp.onrender.com
+// LOGIN https://my-shop-api-n5vp.onrender.com + /api/auth/login
+// REGISTER https://my-shop-api-n5vp.onrender.com + /api/auth/register
